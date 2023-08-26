47da33aad2140c35be3e1bd7e9412df6e7200ed1
Ayko Sousa do Nascimento <ayko.souza74@gmail.com>
2023-08-26 10:08:44 -0300

    setup: Inserindo novas configurações

--- Cypress-Testing/cypress.config.js
+++ Cypress-Testing/cypress.config.js
@@ -1,7 +1,11 @@
-const { defineConfig } = require('cypress')
+const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-    
+    baseUrl: 'https://devfinance-agilizei.netlify.app/#',
+    defaultCommandTimeout: 5000,
+    setupNodeEvents(on, config) {
+      
+    },
   },
-})
+});
--- Cypress-Testing/cypress/e2e/spec.cy.js
+++ Cypress-Testing/cypress/e2e/spec.cy.js
@@ -5,7 +5,7 @@ import  { format, prepareLocalStorage } from '../support/utils'
 context("Dev Finances Agilizei", () => {
 
   beforeEach(() => {
-    cy.visit('https://devfinance-agilizei.netlify.app/#')
+    cy.visit('/')
   });
 
 
