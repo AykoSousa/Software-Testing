8205b100549ca3b3556f480236bc04970f25a3ea
Ayko Sousa do Nascimento <ayko.souza74@gmail.com>
2023-08-26 03:11:25 -0300

    setup: Preprando o ambiente de testes

---
+++ Cypress-Testing/cypress/e2e/spec.cy.js
@@ -0,0 +1,15 @@
+/// <reference types="cypress"/>
+
+import  { format, prepareLocalStorage } from '../support/utils'
+
+context("Dev Finances Agilizei", () => {
+
+  beforeEach(() => {
+    cy.visit('https://devfinance-agilizei.netlify.app/#')
+  });
+
+
+  it('Teste',() => {
+    
+  })
+});
