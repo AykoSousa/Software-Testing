5f6dcb0ba6732724d0d5d1a70f48bd0aa9258c64
Ayko Sousa do Nascimento <ayko.souza74@gmail.com>
2023-09-05 20:32:37 -0300

    Config: Remoção de comentários

--- Cypress-Testing/cypress.config.js
+++ Cypress-Testing/cypress.config.js
@@ -4,7 +4,6 @@ module.exports = defineConfig({
   projectId: '6k4ouh',
   e2e: {
     baseUrl: 'https://devfinance-agilizei.netlify.app/#',
-    //defaultCommandTimeout: 2000,
     setupNodeEvents(on, config) {
       
     },
