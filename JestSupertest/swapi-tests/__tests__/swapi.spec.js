e9471cf08d5448405a3c4e7b649fdd3b11042c6d
Ayko Sousa do Nascimento <ayko.souza74@gmail.com>
2023-10-08 13:42:57 -0300

    Feat: Implementando testes na api

--- JestSupertest/swapi-tests/__tests__/swapi.spec.js
+++ JestSupertest/swapi-tests/__tests__/swapi.spec.js
@@ -1,5 +1,20 @@
 const request = require('supertest')
 
-it('', ()=> {
-  
-})
+it('Deve visualizar informações de cadastro, quando buscar por uma pessoa existente', async ()=> {
+
+  const response = await request(`https://swapi.dev/api`).get(`/people/1`);
+
+  expect(response.status).toBe(200);
+  expect(response.body.films).toBeDefined();
+  expect(response.body.vehicles.length).toBeGreaterThan(0);
+  expect(response.body.name).toBe('Luke Skywalker')
+});
+
+it('Deve receber uma mensagem de erro, quando buscar por uma pessoa inexistente', async ()=> {
+    const response = await request(`https://swapi.dev/api`).get(`/people/999`);
+
+    expect(response.status).toBe(404);
+    expect(response.body).toMatchObject({
+        detail: 'Not found'
+    });
+});
